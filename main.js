a94d8178bde903cc8e5506d11a281d35a3c1eac7
GustafJ <gustaf89@hotmail.se>
2020-11-08 01:06:07 +0100

    eraser my name in aboutMe

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ class Main {
     let aboutMe = document.getElementsByClassName("aboutMe")[0];
     let portfolio = document.getElementsByClassName("portfolio")[0];
     let contactMe = document.getElementsByClassName("contactMe")[0];
+    let myName = document.getElementById("myName");
     menuList.addEventListener("click", (e) => {
       aboutMe.classList.remove("aboutMePicked");
       portfolio.classList.remove("portfolioPicked");
@@ -16,9 +17,11 @@ class Main {
         "portfolioBackground",
         "contactMeBackground"
       );
+      myName.classList.remove("hidden");
       if (e.target == aboutMe) {
         mainContentBackground.classList.add("aboutMeBackground");
         aboutMe.classList.add("aboutMePicked");
+        myName.classList.add("hidden");
       } else if (e.target === portfolio) {
         mainContentBackground.classList.add("portfolioBackground");
         portfolio.classList.add("portfolioPicked");
