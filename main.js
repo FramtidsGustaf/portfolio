822213f42fe789203d8e0314e9ccf843a36b8f27
GustafJ <gustaf89@hotmail.se>
2020-11-09 13:04:30 +0100

    tried out some design

--- main.js
+++ main.js
@@ -3,12 +3,14 @@ class Main {
     this.menuPick();
   }
   menuPick() {
+    let main = document.getElementsByClassName("main")[0];
     let mainContentBackground = document.getElementsByClassName("mainContent")[0];
     let aboutMe = document.getElementsByClassName("aboutMe")[0];
     let portfolio = document.getElementsByClassName("portfolio")[0];
     let contactMe = document.getElementsByClassName("contactMe")[0];
     let myName = document.getElementById("myName");
     let puppiePicture = document.getElementsByClassName("puppiePicture")[0];
+
     menuList.addEventListener("click", (e) => {
       aboutMe.classList.remove("aboutMePicked");
       portfolio.classList.remove("portfolioPicked");
@@ -22,6 +24,7 @@ class Main {
         puppiePicture.classList.add("hidden");
       }
       myName.classList.remove("hidden");
+
       if (e.target == aboutMe) {
         mainContentBackground.classList.add("aboutMeBackground");
         aboutMe.classList.add("aboutMePicked");
